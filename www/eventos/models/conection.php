b755023e46e553a046fe3316e641e00802160ad4
Mariano Graziano <mariano.graziano@gmail.com>
2024-03-18 18:37:26 -0300

    Mysql Connection Fixed

--- www/eventos/models/conection.php
+++ www/eventos/models/conection.php
@@ -3,8 +3,8 @@
 class Conexion{
     static public function conectar(){
         try {
-            $conn = new PDO("mysql:host=localhost;dbname=market-pos", "root", "",array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-            return $conn; 
+            $conn = mysqli_connect('db', 'root', 'test', "market-pos");
+            return $conn;
         }
         catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
--- www/eventos/models/dashboard-train.model.php
+++ www/eventos/models/dashboard-train.model.php
@@ -3,9 +3,8 @@
 require_once "conection.php";
 class DashboardTrainModel{
     static public function mdlGetDatosDashboardTrain(){
-        $stmt = Conexion::conectar()->prepare("call prc_ObtenerDatosDashboard()");
-        $stmt->execute();
-        return $stmt->fetchAll();
-
+        $query = 'call prc_ObtenerDatosDashboard()';
+        $result = mysqli_query(Conexion::conectar(), $query);
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 }
